a6133ba89da3d1bd2ecfdd21109fa4afed42e35b
Perelyn-sama <64838956+Perelyn-sama@users.noreply.github.com>
2022-04-20 15:53:58 +0100

    added comments

--- foundry-version/src/Lottery.sol
+++ foundry-version/src/Lottery.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
+// ReentracncyGuard implementation from open zeppelin 
 abstract contract ReentrancyGuard {
     // Booleans are more expensive than uint256 or any type that takes up a full
     // word because each write operation emits an extra SLOAD to first read the
@@ -45,19 +46,32 @@ abstract contract ReentrancyGuard {
 }
 
 contract Lottery is ReentrancyGuard {
+
+    // To store the guesses players made
     mapping(address => uint256) public guesses;
+
+    // Fee to play the lottery
     uint256 public fee = 1 ether;
+
+    // To store the addresses that play the game
     address[] public players;
+
+    // To store the addresses that got the guess right 
     address[] public winners;
+
+    // Made this so i can grant withdrawal right to just the winners addresses
     mapping(address => bool) public canWithdraw;
 
+    // Not really needed, was testing something on remix
     constructor() payable {}
 
+    // Grants access only to winners
     modifier onlyWinner() {
-        require(canWithdraw[msg.sender] == true);
+        require(canWithdraw[msg.sender] == true), "This address is not a winner";
         _;
     }
 
+    // Play the game 
     function takeAGuess(uint256 guess) public payable {
         require(guess <= 20, "Guess between 1 - 20");
         require(msg.value == fee, "Not enough funds to take a guess");
@@ -67,10 +81,13 @@ contract Lottery is ReentrancyGuard {
         players.push(msg.sender);
     }
 
+    // Get balance of the contract
     function contractBalance() public view returns (uint256) {
         return address(this).balance;
     }
 
+    // Check if the players got the guess right
+    // push the address of those who got the guess right to the winners array
     function check() public {
         require(address(this).balance >= 3 ether, "Game not yet over");
         uint256 num = 5;
@@ -82,6 +99,8 @@ contract Lottery is ReentrancyGuard {
         }
     }
 
+    // Can only be called by addresses in the winners array
+    // Sends the ether in the contract to the winners
     function withdraw() public onlyWinner nonReentrant {
         uint256 bal = address(this).balance;
         uint256 share = bal / winners.length;
@@ -90,12 +109,13 @@ contract Lottery is ReentrancyGuard {
             _withdraw(x, share);
         }
     }
-
+    // Internal function for the withdraw logic
     function _withdraw(address _address, uint256 _amount) internal {
         (bool success, ) = _address.call{value: _amount}("");
         require(success, "WITHDRAW: Transfer failed.");
     }
 
+    // Check the balance of the the address passed in
     function balanceOf(address acct) public view returns (uint256) {
         return acct.balance;
     }
